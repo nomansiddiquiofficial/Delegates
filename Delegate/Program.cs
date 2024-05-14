55339f4073458762e81aadcc3e83faa13a0401db
Noman Siddiqui <92144508+nomansiddiquiofficial@users.noreply.github.com>
2024-05-14 11:31:23 +0500

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,49 @@
+﻿// Delegates 
+
+public delegate void delegateForCalculateWIthoutParameter();
+public delegate void delegateForcalculateSquareWithParameter(int a);
+public delegate void delegateForCalculateSubstractWithParameter(int a, int b);
+
+class MathCalculation
+{
+    public static void calculateWIthoutParameter()
+    {
+        Console.WriteLine("this is the function made for learning delegate without parameter");
+    }
+    public static void calculateWIthoutParameter2()
+    {
+        Console.WriteLine("this is the function2 made for learning delegate without parameter");
+    }
+    public static void calculateSquareWithParameter(int num)
+    {
+        int result = num * num;
+        Console.WriteLine($"the square of num is: {result}");
+    }
+    public static void calculateSubstractWithParameter(int num1, int num2)
+    {
+        int result = Math.Abs(num1 - num2);
+        Console.WriteLine($"the substract of numbers is: {result}");
+    }
+
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        delegateForCalculateWIthoutParameter delegate1 = new delegateForCalculateWIthoutParameter(MathCalculation.calculateWIthoutParameter);
+        //delegate1.Invoke();
+
+        delegate1 = MathCalculation.calculateWIthoutParameter2;
+        delegate1.Invoke();
+
+        delegateForcalculateSquareWithParameter delegate2 = new delegateForcalculateSquareWithParameter(MathCalculation.calculateSquareWithParameter);
+        delegate2.Invoke(2);
+
+        delegateForCalculateSubstractWithParameter delegate3 = new delegateForCalculateSubstractWithParameter(MathCalculation.calculateSubstractWithParameter);
+        delegate3(5,1);
+
+    }
+}
